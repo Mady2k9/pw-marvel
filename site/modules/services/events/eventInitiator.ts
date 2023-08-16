3428ef480aab7e471ae87fb968f3825c9770346d
Ujjwal <ujjwal@pw.live>
2023-08-16 10:19:34 +0530

    add utms in events

--- site/modules/services/events/eventInitiator.ts
+++ site/modules/services/events/eventInitiator.ts
@@ -26,9 +26,30 @@ userId = helper.isUserloggedIn()
   ? JSON.parse(localStorage?.getItem('user') || '{}').id
   : ''
 
-source = globalProps.utmParams?.utm_source || ''
+/* source = globalProps.utmParams?.utm_source || ''
 medium = globalProps.utmParams?.utm_medium || ''
-campaign = globalProps.utmParams?.utm_campaign || ''
+campaign = globalProps.utmParams?.utm_campaign || '' */
+
+export const getUserData = () => {
+  const utmData = globalProps.utmParams
+
+  //localStorage.setItem('UTM_KEY', JSON.stringify(utmData))
+
+  const data: Record<string, any> = {}
+
+  data['utm_source'] = globalProps.utmParams?.utm_source || ''
+  data['utm_medium'] = globalProps.utmParams?.utm_medium || ''
+  data['utm_campaign'] = globalProps.utmParams?.utm_campaign || ''
+
+  return data
+}
+
+export const trackGaEvent = (eventName: any, body: any) => {
+  const data = getUserData()
+  const updatedData = { ...body, ...data }
+
+  gtag('event', eventName, updatedData)
+}
 
 const triggerTrackEvent = {
   marvelLandingPage: () => {
@@ -37,12 +58,9 @@ const triggerTrackEvent = {
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
       DeviceId: deviceId,
-      utm_source: source,
-      utm_medium: medium,
-      utm_campaign: campaign,
       user_type: helper.isUserloggedIn() ? 'logged_in' : 'not_logged_in',
     }
-    gtag('event', EventType.MARVEL_LANDING_PAGE, body)
+    trackGaEvent(EventType.MARVEL_LANDING_PAGE, body)
   },
 
   marvelRegisterNow: ({ coming_from }: marvelRegisterNowProps) => {
@@ -50,13 +68,10 @@ const triggerTrackEvent = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       coming_from: coming_from,
     }
-    //logEvent(EventType.MARVEL_REGISTER_NOW, body, false)
-    gtag('event', EventType.MARVEL_REGISTER_NOW, body)
+
+    trackGaEvent(EventType.MARVEL_REGISTER_NOW, body)
   },
 
   marvelRegisterNumber: (phone_number: string) => {
@@ -64,13 +79,9 @@ const triggerTrackEvent = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       UserPhoneNumber: phone_number,
     }
-    //logEvent(EventType.MARVEL_MOBILE_NUMBER, body, false)
-    gtag('event', EventType.MARVEL_MOBILE_NUMBER, body)
+    trackGaEvent(EventType.MARVEL_MOBILE_NUMBER, body)
   },
 
   marvelRegisterSuccess: (phone_number: string) => {
@@ -78,13 +89,9 @@ const triggerTrackEvent = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       UserPhoneNumber: phone_number,
     }
-    //logEvent(EventType.MARVEL_REGISTER_SUCCESS, body, false)
-    gtag('event', EventType.MARVEL_REGISTER_SUCCESS, body)
+    trackGaEvent(EventType.MARVEL_REGISTER_SUCCESS, body)
   },
 
   marvelNavbarNavigation: (navigation_name: string) => {
@@ -92,76 +99,61 @@ const triggerTrackEvent = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       navigation_name: navigation_name,
       user_type: helper.isUserloggedIn() ? 'logged_in' : 'not_logged_in',
     }
-    //logEvent(EventType.MARVEL_NAVBAR_NAVIGATION, body, false)
-    gtag('event', EventType.MARVEL_NAVBAR_NAVIGATION, body)
+    trackGaEvent(EventType.MARVEL_NAVBAR_NAVIGATION, body)
   },
 
   marvelLoginNow: ({ coming_from }: marvelLoginNowProps) => {
+    // const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       coming_from: coming_from,
     }
-    //logEvent(EventType.MARVEL_LOGIN_PAGE, body, false)
-    gtag('event', EventType.MARVEL_LOGIN_PAGE, body)
+    trackGaEvent(EventType.MARVEL_LOGIN_PAGE, body)
   },
 
   marvelLoginNumber: (phone_number: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       UserPhoneNumber: phone_number,
     }
-    //logEvent(EventType.MARVEL_MOBILE_NUMBER_CLICK, body, false)
-    gtag('event', EventType.MARVEL_MOBILE_NUMBER_CLICK, body)
+    trackGaEvent(EventType.MARVEL_MOBILE_NUMBER_CLICK, body)
   },
   marvelLoginSuccess: (phone_number: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       UserPhoneNumber: phone_number,
     }
-    //logEvent(EventType.MARVEL_LOGIN_SUCCESS, body, false)
-    gtag('event', EventType.MARVEL_LOGIN_SUCCESS, body)
+    trackGaEvent(EventType.MARVEL_LOGIN_SUCCESS, body)
   },
 
   marvelFormVisit: ({ profile_details, form_name }: marvelFormVisitProps) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
       profile_details: profile_details,
       form_name: form_name,
     }
-    //logEvent(EventType.MARVEL_FORM_VISIT, body, false)
-    gtag('event', EventType.MARVEL_FORM_VISIT, body)
+    trackGaEvent(EventType.MARVEL_FORM_VISIT, body)
   },
 
   marvelSubmitProfile: ({
     std_class,
     profile_type,
   }: marvelSubmitProfileProps) => {
+    // const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
@@ -169,55 +161,48 @@ const triggerTrackEvent = {
       class: std_class,
       type: profile_type,
     }
-    //logEvent(EventType.MARVEL_NOMINATION_TNC_POPUP, body, false)
-    gtag('event', EventType.MARVEL_SUBMIT_PROFILE, body)
+    trackGaEvent(EventType.MARVEL_SUBMIT_PROFILE, body)
   },
 
   marvelNominationTncPopup: (std_class: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
       class: std_class,
     }
-    //logEvent(EventType.MARVEL_NOMINATION_TNC_POPUP, body, false)
-    gtag('event', EventType.MARVEL_NOMINATION_TNC_POPUP, body)
+    trackGaEvent(EventType.MARVEL_NOMINATION_TNC_POPUP, body)
   },
   marvelNominationTncAction: (std_class: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
       class: std_class,
     }
-    //logEvent(EventType.MARVEL_NOMINATION_TNC_ACTION, body, false)
     gtag('event', EventType.MARVEL_NOMINATION_TNC_ACTION, body)
   },
   marvelNominationSubmit: (std_class: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
       class: std_class,
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
     }
-    //logEvent(EventType.MARVEL_NOMINATION_SUBMIT, body, false)
-    gtag('event', EventType.MARVEL_NOMINATION_SUBMIT, body)
+    trackGaEvent(EventType.MARVEL_NOMINATION_SUBMIT, body)
   },
   marvelDocumentSubmit: (std_class: string) => {
+    //const data = getUserData()
     const body = {
       UserId: helper.isUserloggedIn()
         ? JSON.parse(localStorage?.getItem('user') || '{}').id
         : '',
       class: std_class,
-      utm_source: source,
-      utm_campaign: campaign,
-      utm_medium: medium,
     }
-    //logEvent(EventType.MARVEL_DOCUMENT_SUBMIT, body, false)
-    gtag('event', EventType.MARVEL_DOCUMENT_SUBMIT, body)
+    trackGaEvent(EventType.MARVEL_DOCUMENT_SUBMIT, body)
   },
 }
 
